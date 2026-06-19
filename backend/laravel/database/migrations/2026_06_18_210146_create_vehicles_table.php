ed18aaf619afd9ab33b4bc95cc7aa26f9f704983
mamun441998 <mamun441998@gmail.com>
2026-06-20 03:00:33 +0600

    Add vehicle identity migration structure

--- backend/laravel/database/migrations/2026_06_18_210146_create_vehicles_table.php
+++ backend/laravel/database/migrations/2026_06_18_210146_create_vehicles_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
+            $table->uuid("uuid")->unique();
+            $table->string("slug")->unique(); 
             $table->timestamps();
+            $table->softdeletes();
         });
     }
 
